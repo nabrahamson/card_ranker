f5fd6339b13446ff4f8a253baf3540df678e974d
Noah Abrahamson <Noah.Abrahamson@cunamutual.com>
2018-03-29 16:40:25 -0500

    Complete decision tree for ranking a poker hand

--- ranker.js
+++ ranker.js
@@ -1,4 +1,4 @@
-import (countBy, uniq, map, has) from 'lodash';
+import (countBy, each, has, invert, map, uniq) from 'lodash';
 
 const highCards = {
   A: 13,
@@ -8,12 +8,76 @@ const highCards = {
 }
 
 const rank_hand = function rank(hand) {
+  let flushSuit;
+  let isStraight = false;
+  let hasAceKing = false;
+  let sets = {};
+  let handType;
+
+  const has = Object.prototype.hasOwnProperty;
+  const handOutput = 'Hand: ' + hand + ' ';
+
   const suits = map(hand, card => {
     return card[1];
   });
   const ranks = map(hand, card => {
     return card[0];
   });
+
+  // Check for flush or straight
+  flushSuit = is_flush(suits);
+  isStraight = is_straight(ranks);
+
+  // If we have a flush, or straight, there are no pairs
+  if (is_flush || isStraight) {
+    // if the hand is a straight, check if it's 10, J, Q, K, A
+    if (isStraight) {
+      hasAceKing = has_ace_king(ranks);
+    }
+
+    if(flushSuit && hasAceKing) {
+      // Flush in the sequence 10, J, Q, K, A
+      handType = '(Royal Flush)';
+    } else if(flushSuit && isStraight) {
+      handType = '(Straight Flush)';
+    } else if (isStraight) {
+      handType = '(Straight)';
+    } else if (flushSuit) {
+      handType = '(Flush)';
+    }
+  } else {
+
+    // Look for pairs
+    sets = check_sets(ranks);
+    if (has.call(sets, 'quad')) {
+      handType = '(Four of a kind ';
+      handType += get_rank(sets.quad);
+      handType += ')';
+    } else if (has.call(sets, 'triple')) {
+      if (has.call(sets, 'pairs')) {
+        handType = '(Full House)';
+      } else {
+        handType = '(Three of a kind ';
+        handType += get_rank(sets.triple);
+        handType += ')';
+      }
+    } else if (has.call(sets, 'pairs')) {
+      if (sets.pairs.length === 2) {
+        handType = '(2 Pair)';
+      } else {
+        handType = '(Pair of ';
+        handType += get_rank(sets.pairs[0]);
+        handType += ')';
+      }
+    } else {
+      //This is a high card hand
+      handType = '(High card ';
+      handType += high_card(ranks);
+      handType += ')';
+    }
+
+    // print ranking to console
+    console.log(handOutput + handType);
 }
 
 const is_flush = function flush(suits) {
@@ -22,16 +86,6 @@ const is_flush = function flush(suits) {
     return uniqueSuits[0];
   }
   return null;
-}
-
-const has_pairs = function pairs(ranks) {
-  const rankCount = countBy(ranks);
-  if(rankCount.length === 5) {
-    return false;
-  }
-  if(rankCount.length === 4) {
-    return check_sets(rankCount);
-  }
 }
 
 const is_straight = function straight(ranks) {
@@ -50,9 +104,60 @@ const is_straight = function straight(ranks) {
   return false;
 };
 
-const is_three_of_kind;
-const is_four_of_kind;
-const high_card;
+const has_ace_king = function ace_king(ranks) {
+  if (ranks.includes('A') && ranks.includes('K')) {
+    return true;
+  }
+  return false;
+}
+
+
+const check_sets = function set_check(ranks) {
+  let pairs = [];
+  let triple;
+  let quad;
+  let sets = {};
+  const rankCount = countBy(ranks);
+
+  each(Object.keys(rankMap), key => {
+    if (rankMap[key] === 2) {
+      pairs.push(key);
+    } else if (rankMap[key] === 3) {
+      triple = key;
+    } else if (rankMap[key] === 4) {
+      quad = key;
+    }
+  })
+
+  if (pairs.length > 0) {
+    sets['pairs'] = pairs;
+  }
+  if (triple) {
+    sets['triple'] = triple;
+  }
+  if (quad) {
+    sets['quad'] = quad;
+  }
+
+  return sets;
+
+}
+
+const get_rank = function string_rank(value) {
+  if (value > 10) {
+    return (invert(highCards))[value];
+  }
+  return value;
+}
+
+const high_card = function high(ranks) {
+
+  const highCard = (ranks.sort())[4];
+  if (highCard > 10) {
+    return (invert(highCards))[highCard];
+  }
+  return highCard;
+}
 
 const enumerate_ranks = function enumerate(ranks) {
   const numericRanks = map(ranks, card => {
@@ -61,4 +166,6 @@ const enumerate_ranks = function enumerate(ranks) {
     }
     return parseInt(card);
   });
+
+  return numericRanks.sort();
 }
